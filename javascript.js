f4203ac9dd8c444ea1ed451908635dabdd76637d
Anderson Eduardo <anderson.eduardoap@gmail.com>
2025-03-04 11:54:57 -0300

    Adicionando icone+link do github/linkedin no rodapé

--- javascript.js
+++ javascript.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
     {
       title: "Análise e Desenvolvimento de Sistemas",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="card-icon green"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>`,
-      duration: "2-3 anos",
+      duration: "2 anos",
       focus: "Software, programação, banco de dados, desenvolvimento web/mobile",
       jobOpportunities: "Empresas de tecnologia, startups, consultorias",
       salary: "R$ 3.500 - R$ 10.000",
